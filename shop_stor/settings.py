8030fd3ce165eff960e49238364bbe9f2f2eb5ec
yousefalshrgabi <yousef4.4.2004.com@gmail.com>
2026-05-21 03:45:28 +0800

    2 commit

--- shop_stor/settings.py
+++ shop_stor/settings.py
@@ -150,3 +150,6 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10,
 }
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
