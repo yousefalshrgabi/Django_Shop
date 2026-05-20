83313b43a97e95659d93728fdf787d994e5f1e47
yousefalshrgabi <yousef4.4.2004.com@gmail.com>
2026-05-21 06:52:45 +0800

    6

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 # pyrefly: ignore [missing-import]
 from .models import Product
-
+# admin.site.register(Product)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'description')
